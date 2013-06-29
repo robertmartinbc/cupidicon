2433d4e503f08935bcce4a1158e020c189ca103f
Robert Martin <robertmartinbc@shaw.ca>
2013-06-29 09:52:09 -0700

    First commite and README update.

---
+++ README.md
@@ -0,0 +1,11 @@
+== == Welcome to Cupidicon!
+
+Cupidicon will be the first online portal for clients and authors interested purely in content writing based on sentiment.
+
+It will be a portal for likeminded people to meet and enjoy each others work.
+
+Clients can register to request articles to be written for a fee.
+
+Authors can register to write the articles or share their work with others.
+
+Anyone can read the articles in search of inspiration.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cupidicon::Application.config.secret_token = '797481267ece0eac6662a7eead214a384c6f9da570d631f323df5a47fde7f9b70403c55aa22fe14999006d3293e31a568fa92778a53a848ab7dd882379139404'
