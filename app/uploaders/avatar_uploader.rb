6ab6311042183e2194ced7a6043b9a73b40e005c
Robert Martin <robertmartinbc@shaw.ca>
2013-07-09 20:25:00 -0700

    Modified Carrierwave settings.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,6 +2,10 @@
 
 class AvatarUploader < CarrierWave::Uploader::Base
 
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
