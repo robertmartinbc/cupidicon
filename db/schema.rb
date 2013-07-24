fe67faad3ab884f82a32ec288d2a8dfec4db73ce
Robert J Martin <robertmartinbc@shaw.ca>
2013-07-24 14:01:45 -0400

    Changed postgres password.

--- db/schema.rb
+++ db/schema.rb
@@ -40,15 +40,6 @@ ActiveRecord::Schema.define(:version => 20130710015746) do
 
   add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
-  create_table "profiles", :force => true do |t|
-    t.text     "title"
-    t.text     "skills"
-    t.text     "location"
-    t.text     "experience"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
